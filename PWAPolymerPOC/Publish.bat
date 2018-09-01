3d984037ddc16114d35562716e339c63fca0fd9d
Tomas Vilaboa <tvilaboa@hotmail.com>
2018-09-01 17:34:46 -0300

    Change publish

--- PWAPolymerPOC/Publish.bat
+++ PWAPolymerPOC/Publish.bat
@@ -1,5 +1,5 @@
 dotnet publish -c=Release
 cd wwwroot
 call npm install
-call polymer build --preset=es5-bundled
-move build/es5-bundled ../bin/Release/netcoreapp2.0/publish/wwwroot
+call polymer build --preset=es6-bundled
+move build/es6-bundled ../bin/Release/netcoreapp2.1/publish/wwwroot
